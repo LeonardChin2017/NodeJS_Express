abf839215344fdeac5ff0702187567658374fd1a
Leonard Chin <leonardchin2017@gmail.com>
2021-12-27 14:06:01 +0800

    🦄 refactor(router): refactor route code
    refactor router function (get, put, delete)

--- routes/users.js
+++ routes/users.js
@@ -13,10 +13,40 @@ router.post("/", (req, res)=>{
     res.send('Create user')
 })
 
-// dynamic route
-router.get("/:id", (req, res)=>{
-    var userid = req.params.id
-    res.json({message: userid})
-})
+// cleaner way
+router
+    .route("/:id")
+    .get((req, res) =>{
+        var userid = req.params.id
+        res.send("Get user id " + userid)
+    })
+    .put((req, res) =>{
+        var userid = req.params.id
+        res.send("update user id " + userid)
+    })
+    .delete((req, res) => {
+        var userid = req.params.id
+        res.send("delete user id " + userid)
+    })
+
+// // dynamic route || dirty way
+// router.get("/:id", (req, res)=>{
+//     var userid = req.params.id
+//     //res.json({message: userid})
+//     res.send("Get user id " + userid)
+// })
+
+// router.put("/:id", (req, res)=>{
+//     var userid = req.params.id
+//     //res.json({message: userid})
+//     res.send("update user id " + userid)
+// })
+
+
+// router.delete("/:id", (req, res)=>{
+//     var userid = req.params.id
+//     //res.json({message: userid})
+//     res.send("delete user id " + userid)
+// })
 
 module.exports = router
